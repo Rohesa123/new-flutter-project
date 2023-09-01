fd1da20f0013fb6ad5138ba66675af1efd87a606
Rohesa123 <rohesasidiqpermana05@gmail.com>
2023-09-01 09:24:21 +0700

    UPDATE: Refactor Widget

--- lib/main.dart
+++ lib/main.dart
@@ -38,15 +38,26 @@ class Home extends StatelessWidget {
           Icon(Icons.group),
         ],
       ),
-      body: Container(
-        margin: EdgeInsets.fromLTRB(20, 20, 0, 20),
-        padding: EdgeInsets.all(20),
-        color: Colors.indigo,
-        child: Icon(
-          Icons.headset,
-          color: Colors.white,
-          size: 100.0,
-        ),
+      body: Halaman(),
+    );
+  }
+}
+
+class Halaman extends StatelessWidget {
+  const Halaman({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(20, 20, 0, 20),
+      padding: EdgeInsets.all(20),
+      color: Colors.indigo,
+      child: Icon(
+        Icons.headset,
+        color: const Color.fromARGB(255, 172, 168, 168),
+        size: 100.0,
       ),
     );
   }
